e3be8e3afbd69fb20666085fc204b95fa9e6e6e9
daniel1302 <daniel.1302@gmail.com>
2019-05-08 20:24:47 +0000

    Implement nextcloud

--- install.sh
+++ install.sh
@@ -28,9 +28,26 @@ replaceInFile()
 
 # get mysql password
 read -sp 'Mysql password: ' MYSQL_PASSWORD;
+echo "";
+read -p 'Nextcloud username: ' NEXTCLOUD_USER
+read -sp 'Nextcloud password: ' NEXTCLOUD_PASSWORD;
+
+
 
 #replace mysql password in required files
 replaceInFile ./vault/config.sample.hcl ./vault/config.hcl TYPE_MYSQL_PASSWORD_HERE $MYSQL_PASSWORD;
 replaceInFile ./assets/secrets.sample.env ./assets/secrets.env TYPE_MYSQL_PASSWORD_HERE $MYSQL_PASSWORD;
+replaceInFile ./assets/secrets.env ./assets/secrets.env TYPE_NEXTCLOUD_ADMIN_USER_HERE $NEXTCLOUD_USER;
+replaceInFile ./assets/secrets.env ./assets/secrets.env TYPE_NEXT_CLOUD_ADMIN_PASSWORD $NEXTCLOUD_PASSWORD;
+
+#Create nextcloud directories if not exists
+if [ ! -e ./nextcloud ]; then
+    mkdir -p ./nextcloud/apps;
+    mkdir -p ./nextcloud/config;
+    mkdir -p ./nextcloud/data;
+    mkdir -p ./nextcloud/nextcloud;
+fi;
+
+
 
 echo "Finished"
